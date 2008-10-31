52398836de73ab36f8f6a770fc9d8e9a0933eba1
PJ Eby <pje@telecommunity.com>
2008-10-31 17:20:08 +0000

    Bump version
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/EccoDDE@2588 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 # Metadata
 PROJECT = 'EccoDDE'
-VERSION = '0.9.1'
+VERSION = '0.9.2'
 TAGLINE = "Thin wrapper over the Ecco Personal Information Manager's DDE API"
 PACKAGES   = []
 NAMESPACES = []
